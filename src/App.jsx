7d28a7aa31b48438da73ac0b13c7fb45a05854fb
Agustin Martinez <agustinmartinezyt@gmail.com>
2024-04-30 18:52:31 -0300

    add useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react"
 import { Footer } from "./components/Footer/Footer"
 import { Header } from "./components/Header/Header"
 import { Main } from "./components/Main/Main.jsx"
 
 function App() {
+
+  //State
+  const [auth, setAuth] = useState({})
+
   return (
     <>
       <Header />
