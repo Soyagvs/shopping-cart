75d0477563a0eff03697ce8e201399e70ad998b9
Agustin Martinez <agustinmartinezyt@gmail.com>
2024-05-01 18:20:10 -0300

    delete useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from "react"
 import { Footer } from "./components/Footer/Footer"
 import { Header } from "./components/Header/Header"
 import { Main } from "./components/Main/Main.jsx"
-import { db } from "./data/db.js"
 
 function App() {
-
-  //useState
-  const [data, setData] = useState(db)
 
   return (
     <>
