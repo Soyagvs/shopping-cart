0ccdc0246c383d9f6324e51177383901b8b4de94
Agustin Martinez <agustinmartinezyt@gmail.com>
2024-05-01 18:20:50 -0300

    use map()

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import { db } from "../../data/db"
 import { Guitarras } from '../Guitarras/Guitarras'
+import { useState } from 'react'
 
 export const Main = () => {
+    //useState
+    const [data, setData] = useState(db)
+
     return (
+
         <main className="container-xl mt-5">
             <h2 className="text-center">Nuestra Colección</h2>
 
             <div className="row mt-5">
-               <Guitarras/>
+                {data.map((guitar) => (
+                    <Guitarras
+                        key={guitar.id}
+                        guitar={guitar}
+                    />
+                ))}
             </div>
         </main>
     )
