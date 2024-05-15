404766a857af0f0ea0fe250bef59d8308b241d88
Soyagvs <agustinmartinezyt@gmail.com>
2024-05-15 18:29:57 -0300

    changes

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,26 +1,21 @@
-import React from 'react'
-
-export const Guitarras = ({ guitar }) => {
+export default function Guitar({guitar, addToCart}) {
 
     const { id, name, image, description, price } = guitar
 
-    const handleClick = (id) => {
-        console.log("Diste click..", id)
-    }
 
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-                <img className="img-fluid" src={`${image}.jpg`} alt="imagen guitarra" />
+                <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
             </div>
             <div className="col-8">
                 <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
                 <p>{description}</p>
                 <p className="fw-black text-primary fs-3">${price}</p>
-                <button
+                <button 
                     type="button"
                     className="btn btn-dark w-100"
-                    onClick={() => handleClick(guitar)}
+                    onClick={() => addToCart(guitar)}
                 >Agregar al Carrito</button>
             </div>
         </div>
